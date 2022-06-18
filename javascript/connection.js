2800a613e1e43ba9600f6a2e1d1bbddf0083ac66
Ellias Ribeiro <56983572+sailleribeiro@users.noreply.github.com>
2022-06-18 17:51:24 -0300

    campos removidos
    campos que não estavam sendo usados, foram removidos

--- javascript/connection.js
+++ javascript/connection.js
@@ -11,13 +11,7 @@ $.ajax({
 
     success: function (data) {
         const token = data['X-Token'];
-        const nome = data['nome'];
-        const id = data['id'];
-        const car = data['car'];
-        const tipo = data['status'];
-        const uf = data['uf'];
-        const municipio = data['municipio'];
-
+       
         $.ajax({
             url: "http://192.168.10.200:8086/propriedade/49978",
             contentType: 'application/json',
